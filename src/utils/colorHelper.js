25a70261a3064d93917a436e071c6d4b15b21af2
Ruoyang Wang <ruoyw.dev@gmail.com>
2021-05-27 16:59:47 -0700

    Color selct now works and can update properly. Looking into complement calculator next

--- src/utils/colorHelper.js
+++ src/utils/colorHelper.js
@@ -1,6 +1,6 @@
 export const validColorCheck = (colorStr) => {
     if (colorStr !== null && colorStr.length === 7) {
-        const pattern = /\b#[0-f][0-f][0-f][0-f][0-f][0-f]\b/
+        const pattern = /#[0-f][0-f][0-f][0-f][0-f][0-f]/
         if (colorStr.match(pattern) !== null) {
             return true
         }
