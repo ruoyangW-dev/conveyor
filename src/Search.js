ac255641bcd73d5d0f89042905626796d052b1af
amendaros <audie.mendaros@moesol.com>
2021-06-22 15:24:21 -0700

    Refactor 'Search' to 'QuickSearch'

--- src/Search.js
+++ src/Search.js
@@ -41,7 +41,7 @@ const HighlightString = ({ searchText, textToHighlight }) => {
   )
 }
 
-export const SearchResults = ({ entries, onLinkClick, location }) => {
+export const SearchPage = ({ entries, onLinkClick, location }) => {
   const searchText = location.pathname.split('/')[2]
 
   return (
@@ -69,7 +69,7 @@ export const SearchResults = ({ entries, onLinkClick, location }) => {
   )
 }
 
-export const Search = ({
+export const QuickSearch = ({
   queryText,
   entries,
   onTextChange,
